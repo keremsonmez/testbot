4b6cfe97e08ca077ae85fd4ecb6c925c9d243854
keremsonmez <44930552+keremsonmez@users.noreply.github.com>
2019-03-29 22:08:09 +0300

    Add files via upload

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,5 @@
 module.exports = member => {
   let guild = member.guild;
   member.send('niye gittin?');
-  guild.defaultChannel.sendMessage(`${member.user.username} gitti.`);
+  guild.defaultChannel.send(`${member.user.username} gitti.`);
 };
