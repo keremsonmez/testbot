5bc0ba7078151f99a2ba46e6f58dd61ee53e8540
keremsonmez <44930552+keremsonmez@users.noreply.github.com>
2019-03-30 20:02:22 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,10 +6,11 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  client.user.setPresence({game:{name:`Yardım komutları İçin ${prefix}yardım | ${client.guilds.size} Sunucuda hizmet vermekte | ${client.users.size} Kullanıcı | Yeni Komutlar ${prefix}yardım | TEST BOTU `, type: 0}});
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
+  var Discord = require("discord.js");
+  var bot = new Discord.Client();
+
+bot.on("ready", () => {
+  console.log("Botumuz şuanda  " + bot.guilds.size + " serverda hizmet veriyor");
+  bot.user.setGame("Subscroob,Like", "https://www.twitch.tv/quadria")
+  
+});
